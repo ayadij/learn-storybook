63203a54755556795a732959aed9a9e728ee2c37
ayadij <ayadijkwel@yahoo.com>
2018-08-17 12:11:46 -0600

    connect to redux

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import TaskList from "./TaskList";
+
+import { PureTaskList } from "./TaskList";
 import { task, actions } from "./Task.stories";
+
 export const defaultTasks = [
   { ...task, id: "1", title: "Task 1" },
   { ...task, id: "2", title: "Task 2" },
@@ -10,15 +12,17 @@ export const defaultTasks = [
   { ...task, id: "5", title: "Task 5" },
   { ...task, id: "6", title: "Task 6" }
 ];
+
 export const withPinnedTasks = [
   ...defaultTasks.slice(0, 5),
   { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" }
 ];
+
 storiesOf("TaskList", module)
   .addDecorator(story => <div style={{ padding: "3rem" }}>{story()}</div>)
-  .add("default", () => <TaskList tasks={defaultTasks} {...actions} />)
+  .add("default", () => <PureTaskList tasks={defaultTasks} {...actions} />)
   .add("withPinnedTasks", () => (
-    <TaskList tasks={withPinnedTasks} {...actions} />
+    <PureTaskList tasks={withPinnedTasks} {...actions} />
   ))
-  .add("loading", () => <TaskList loading tasks={[]} {...actions} />)
-  .add("empty", () => <TaskList tasks={[]} {...actions} />);
+  .add("loading", () => <PureTaskList loading tasks={[]} {...actions} />)
+  .add("empty", () => <PureTaskList tasks={[]} {...actions} />);
